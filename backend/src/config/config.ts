96edba7ce86c1de2f66aca3d356cb3a8d97fecdb
Martin Petrov <mlpetrov@ucdavis.edu>
2020-11-30 01:50:48 -0800

    added tensorflow ip and port numbers to config.ts

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -7,18 +7,28 @@ interface Config {
    port: string,
    db: {
       name: string,
+      ip: string,
       username: string,
       password: string,
       port: string
+   },
+   ts: {
+      ip: string,
+      port: string
    }
-}
+};
 const dev: Config = {
    'port': process.env.BACKEND_PORT_EXTERNAL || 'config error',
    'db': {
       'name': process.env.DB_NAME || 'config error',
       'username': process.env.DB_USERNAME || 'config error',
       'password': process.env.DB_PASSWORD || 'config error',
+      'ip': 'localhost',
       'port': process.env.DB_PORT_EXTERNAL || 'config error'
+   },
+   'ts': {
+      'ip': 'localhost',
+      'port': process.env.TS_PORT_EXTERNAL || 'config error',
    }
 };
 const prod: Config = {
@@ -27,7 +37,12 @@ const prod: Config = {
       'name': process.env.DB_NAME || 'config error',
       'username': process.env.DB_USERNAME || 'config error',
       'password': process.env.DB_PASSWORD || 'config error',
+      'ip': 'db' || 'config error',
       'port': process.env.DB_PORT_INTERNAL || 'config error'
+   },
+   'ts': {
+      'ip': 'ts',
+      'port': process.env.TS_PORT_EXTERNAL || 'config error',
    }
 };
 

21513cc95b9e3de3f983c6a26e6aba1ef1637edc
roy <roydey10@gmail.com>
2020-12-23 22:39:06 -0800

    add testing route for reading package

--- backend/src/routes/dbTestRoute.ts
+++ backend/src/routes/dbTestRoute.ts
@@ -1,50 +1,67 @@
 import express from 'express';
-import Package from '../db/data_models/package'
+import Package from '../db/data_models/package';
 import { Client, Pool } from 'pg';
 import config from '../config/config';
 
-import {readPackage} from '../db/dbOperations'
+import {readPackage} from '../db/dbOperations';
 
 const router = express.Router();
 
 /* Create a postgres pool*/
 const pool = new Pool({
-    user: config.db.username,
-    host: config.db.ip,
-    database: config.db.name,
-    password: config.db.password,
-    port: config.db.port
- });
+   user: config.db.username,
+   host: config.db.ip,
+   database: config.db.name,
+   password: config.db.password,
+   port: config.db.port
+});
  
 
 router.get('/',(req, res, next)=>{
-    res.send('Database Testing Router')
-})
+   res.send('Database Testing Router');
+});
 
 /* Test Create Package */
 router.get('/createPackage',(req, res, next)=>{
-    // Checkout a client from the pool
-    pool.connect((err, client, done) =>{
-        if (err) throw err
-        // Create test package object
-        let testPackage: Package = new Package(1, 'name', 'category', 'description', 'inputs', 'ouputs', ['flag1','flag2']);
-        console.log(testPackage);
-        // Insert package object into package table
-        let id = testPackage.create(client);
-        res.send(`New Package ID: ${id}`);
-    });
-})
+   // Checkout a client from the pool
+   pool.connect((err, client, done) =>{
+      if(err){
+         // Release the client and log the error
+         client.release();
+         console.log(err);
+      } 
+      // Create test package object
+      const testPackage: Package = new Package(1, 'name', 'category', 'description', 'inputs', 'ouputs', ['flag1','flag2']);
+      console.log(testPackage);
+      // Insert package object into package table
+      testPackage.create(client)
+         .then(id=>{
+            res.send(`Created a New Package with ID=${id}`);
+            // Release the client resource
+            client.release();
+         });
+   });
+});
 
 /* Test Read Package */
 router.get('/readPackage',(req, res, next)=>{
-    // Checkout a client from the pool
-    pool.connect((err, client, done) =>{
-        if (err) throw err
-        // Create test package object
-        let id = 5;
-        let resultPackage = readPackage(client, id);
-        res.json(JSON.stringify(resultPackage));
-    });
-})
+   // Checkout a client from the pool
+   pool.connect((err, client, done) =>{
+      if(err){
+         // Release the client and log the error
+         client.release();
+         console.log(err);
+      } 
+      // initialize a package object
+      const id = 14;
+      readPackage(client, id)
+         .then(resultPackage =>{
+            // Release the client resource
+            client.release();
+            res.json(JSON.stringify(resultPackage));
+         });
+      
+   });
+});
 
 export default router;

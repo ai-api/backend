49028f5e7b67b3dc5b698ebb1461ee0e3d758398
roy <roydey10@gmail.com>
2020-12-25 20:34:28 -0800

    fix db throwing error when attempting to create already existing table

--- backend/src/db/loadTables.sql
+++ backend/src/db/loadTables.sql
@@ -1,14 +1,15 @@
-/* \c database*/
+DO $$
+BEGIN
 
 /* Create auth table */
-CREATE TABLE auth (
+CREATE TABLE IF NOT EXISTS auth (
     id SERIAL PRIMARY KEY,
     userId int NOT NULL,
     refreshToken varchar NOT NULL
 );
 
 /* Create user table */
-CREATE TABLE sys_user (
+CREATE TABLE IF NOT EXISTS sys_user (
     id SERIAL PRIMARY KEY,
     username varchar NOT NULL,
     password varchar NOT NULL,
@@ -18,7 +19,7 @@ CREATE TABLE sys_user (
 );
 
 /* Create model table */
-CREATE TABLE package (
+CREATE TABLE IF NOT EXISTS package (
     id SERIAL PRIMARY KEY,
     userId int NOT NULL,
     lastUpdated TIMESTAMP NOT NULL,
@@ -32,21 +33,21 @@ CREATE TABLE package (
 );
 
 /* Create category table */
-CREATE TABLE category (
+CREATE TABLE IF NOT EXISTS category (
     id SERIAL PRIMARY KEY,
     name varchar NOT NULL,
     description varchar NOT NULL
 );
 
 /* Create Flag Table */
-CREATE TABLE flag (
+CREATE TABLE IF NOT EXISTS flag (
     id SERIAL PRIMARY KEY,
     name varchar NOT NULL,
     description varchar NOT NULL
 );
 
 /* Create package-flag table */
-CREATE TABLE package_flag (
+CREATE TABLE IF NOT EXISTS package_flag (
     id SERIAL PRIMARY KEY,
     packageId int NOT NULL,
     flagId int NOT NULL
@@ -62,3 +63,5 @@ DROP TABLE flag;
 DROP TABLE package_flag;
 */
 
+END;
+$$

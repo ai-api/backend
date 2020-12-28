9586a5b5a4df35c661a90ff8cf6e2eb228472a1c
roy <roydey10@gmail.com>
2020-12-27 21:46:25 -0800

    restructure package object

--- backend/src/db/data_models/package.ts
+++ backend/src/db/data_models/package.ts
@@ -1,19 +1,24 @@
-import {createPackage, updatePackage, remove} from '../dbOperations';
+import { PoolClient } from 'pg';
+import {dbCreate, dbReadById, dbUpdate, dbRemove} from '../dbOperations';
 import TableNames from '../enums/tableNames';
-interface Package {
-    id?: number;
-    userId: number;
-    lastUpdated?: Date;
-    numApiCalls?: number;
-    name: string;
-    category: string;
-    description: string;
-    input: string;
-    output: string;
-    markdown?: string;
-}
-
 class Package {
+   // TODO: write static get and create functions
+   // TODO: combine create and update functions into a save function
+   // TODO: Find way to keep tack of fields that need to be updated
+   // TODO: Pass client into constructor
+   // TODO: save category as category id rather than string
+   private client: PoolClient;
+   private tableName: string;
+   private id: number;
+   private userId: number;
+   private lastUpdated: Date;
+   private numApiCalls: number;
+   private name: string;
+   private category: number;
+   private description: string;
+   private input: string;
+   private output: string;
+   private markdown: string;
    /*
     * @userId: The user's unique ID number
     * @name: The name of the package
@@ -26,8 +31,8 @@ class Package {
     * @numApiCalls: (Optional) Number of times this package's model was requested. Defaults to 0
     * @markdown: (Optional) markdown file. Defaults to empty string
     */
-   constructor(userId: number, name: string, category: string, description: string, input: string, 
-      output: string, id = -1, lastUpdated = new Date(), numApiCalls = 0, markdown = ''){
+   constructor(client: PoolClient, userId: number, name: string, category: number, description: string, input: string, 
+      output: string, markdown = '', id = -1, lastUpdated = new Date(), numApiCalls = 0){
       this.id = id;
       this.userId = userId;
       this.lastUpdated = lastUpdated;
@@ -38,24 +43,154 @@ class Package {
       this.input = input;
       this.output = output;
       this.markdown = markdown;
+      this.client = client;
+      this.tableName = TableNames.PACKAGE;
    }
-   /*
-     * Creates a new package entry in the package table using
-     * the fields in the current object
-     * @client: The postgres client object
-     * Return: None
-     */
-   async create(client: any): Promise<number>{
-      return await createPackage(client, this);
+
+   public static async readInstance(client: PoolClient, id:number): Promise<Package|null>{
+      const data = await dbReadById(client, TableNames.PACKAGE, id);
+      if(data)
+         return new Package(client, data.userid, data.name, data.category, data.description, data.input,
+            data.output, data.markdown, data.id, data.lastupdated, data.numapicalls);
+      return null;
    }
-   async update(client: any): Promise<number>{
-      return await updatePackage(client, this); 
+
+   public static createInstance(client: PoolClient, userId: number, name: string, category: number, description: string, 
+      input: string, output: string, markdown = ''): Package|null{
+
+      if(client && userId && name && category && description && input && output)
+         return new Package(client, userId, name, category, description, input, output, markdown);
+      return null;
    }
-   async delete(client: any): Promise<number>{
+
+   public async save(): Promise<number>{
+      if(this.id)
+         return await this.update();
+      return await this.insert();
+   }
+   /**
+    * Creates a new package entry in the package table using
+    * the fields in the current object
+    * @param client The postgres client object
+    * Return: None
+    */
+   private async insert(): Promise<number>{
+      return 0;
+   }
+   private async update(): Promise<number>{
+      return 0;
+   }
+   public async delete(): Promise<number>{
       if(this.id){
-         return await remove(client, TableNames.PACKAGE, this.id);
+         return await dbRemove(this.client, this.tableName, this.id);
+      }
+      return -1;
+   }
+
+   
+   public getId(): number{
+      return this.id;
+   }
+
+   public getUserId(): number{
+      return this.userId;
+   }
+
+   public setUserId(newId: number): number{
+      if(newId >= 1){
+         this.userId = newId;
+         return 0;
+      }
+      return -1;
+   }
+
+   public getLastUpdated(): Date{
+      return this.lastUpdated;
+   }
+
+   public getNumApiCalls(): number{
+      return this.numApiCalls;
+   }
+
+   public getName(): string{
+      return this.name;
+   }
+
+   public setName(newName: string): number{
+      if(newName){
+         this.name = newName;
+         return 0;
+      }
+      return -1; 
+   }
+
+   public getCategory(): string{
+      return 'TODO';
+   }
+
+   public setCategory(): number{
+      //TODO
+      return 0;
+   }
+
+   public getDescription(): string{
+      return this.description;
+   }
+
+   public setDescription(newDescription: string): number{
+      if(newDescription){
+         this.description = newDescription;
+         return 0;
+      }
+      return -1;
+   }
+
+   public getInput(): string{
+      return this.input;
+   }
+
+   public setInput(newInput: string): number{
+      if(newInput){
+         this.input = newInput;
+         return 0;
+      }
+      return -1;
+   }
+
+   public getOutput(): string{
+      return this.output;
+   }
+
+   public setOutput(newOutput: string): number{
+      if(newOutput){
+         this.output = newOutput;
+         return 0;
+      }
+      return -1;
+   }
+
+   public getMarkdown(): string{
+      return this.markdown;
+   }
+
+   public setMarkdown(newMarkdown:string): number{
+      if(newMarkdown){
+         this.markdown = newMarkdown;
+         return 0;
       }
       return -1;
    }
 }
+// private client: PoolClient;
+// private tableName: string;
+// private id?: number;
+// private userId: number;
+// private lastUpdated?: Date;
+// private numApiCalls?: number;
+// private name: string;
+// private category: string;
+// private description: string;
+// private input: string;
+// private output: string;
+// private markdown?: string;
 export default Package;

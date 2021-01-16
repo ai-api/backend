b58fbb5089c7cdf01849888760556f36be067095
roy <roydey10@gmail.com>
2021-01-15 21:28:50 -0800

    insert category and flag entries

--- backend/src/db/loadTables.sql
+++ backend/src/db/loadTables.sql
@@ -32,19 +32,59 @@ CREATE TABLE IF NOT EXISTS package (
     markdown TEXT /* no size limit so need to check file size before insertion */
 );
 
-/* Create category table */
-CREATE TABLE IF NOT EXISTS category (
-    id SERIAL PRIMARY KEY,
-    name varchar NOT NULL,
-    description varchar NOT NULL
-);
+IF NOT EXISTS (
+   SELECT 1
+   FROM   information_schema.tables 
+   WHERE  table_schema = 'public'   
+   AND    table_name = 'category') THEN
 
-/* Create Flag Table */
-CREATE TABLE IF NOT EXISTS flag (
-    id SERIAL PRIMARY KEY,
-    name varchar NOT NULL,
-    description varchar NOT NULL
-);
+    /* Create Category Table */
+    CREATE TABLE category (
+        id SERIAL PRIMARY KEY,
+        name varchar NOT NULL,
+        description varchar
+    );
+
+    INSERT INTO category(name, description) VALUES ('image', '');
+    INSERT INTO category(name, description) VALUES ('text', '');
+    INSERT INTO category(name, description) VALUES ('video', '');
+    INSERT INTO category(name, description) VALUES ('audio', '');
+END IF;
+
+IF NOT EXISTS (
+   SELECT 1
+   FROM   information_schema.tables 
+   WHERE  table_schema = 'public'   
+   AND    table_name = 'flag') THEN
+
+    /* Create Flag Table */
+    CREATE TABLE flag (
+        id SERIAL PRIMARY KEY,
+        name varchar NOT NULL,
+        description varchar
+    );
+
+    INSERT INTO flag(name, description) VALUES ('embedding', '');
+    INSERT INTO flag(name, description) VALUES ('language', '');
+    INSERT INTO flag(name, description) VALUES ('generation', '');
+    INSERT INTO flag(name, description) VALUES ('text retrieval', '');
+    INSERT INTO flag(name, description) VALUES ('question answering', '');
+    INSERT INTO flag(name, description) VALUES ('classification', '');
+    INSERT INTO flag(name, description) VALUES ('feature vector', '');
+    INSERT INTO flag(name, description) VALUES ('object detection', '');
+    INSERT INTO flag(name, description) VALUES ('segmentation', '');
+    INSERT INTO flag(name, description) VALUES ('generator', '');
+    INSERT INTO flag(name, description) VALUES ('pose detection', '');
+    INSERT INTO flag(name, description) VALUES ('rnn agent', '');
+    INSERT INTO flag(name, description) VALUES ('augmentation', '');
+    INSERT INTO flag(name, description) VALUES ('style transfer', '');
+    INSERT INTO flag(name, description) VALUES ('resolution', '');
+    INSERT INTO flag(name, description) VALUES ('estimation', '');
+    INSERT INTO flag(name, description) VALUES ('command detection', '');
+    INSERT INTO flag(name, description) VALUES ('pitch extraction', '');
+    INSERT INTO flag(name, description) VALUES ('event classification', '');
+    INSERT INTO flag(name, description) VALUES ('other', '');
+END IF;
 
 /* Create package-flag table */
 CREATE TABLE IF NOT EXISTS package_flag (
